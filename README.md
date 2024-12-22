fadae7196b8a4cc9274c568cbce9c7a555108653
whocares <flevn@protonmail.com>
2024-12-22 02:42:22 +0100

    Minor fix

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # dumb
 
+## TASKS
 
+- Understand how X11 works and write about it (high level overview)
+- Understand how hot reloading works and write about it
--- code/dumb_game_logic.cpp
+++ code/dumb_game_logic.cpp
@@ -2,12 +2,12 @@
 
 #include "dumb_gnulinux_x11.h"
 
-extern "C" void game_render(Display *display, Window window, GC gc) {
+extern "C" U0 game_render(Display *display, Window window, GC gc) {
   XClearWindow(display, window);
   XSetForeground(display, gc, 0x000000);
   XFillRectangle(display, window, gc, 100, 100, 200, 200);
 }
 
-extern "C" void game_update() {
+extern "C" U0 game_update() {
 
 }
