18ea5c3467900d65934008e47ea94b35d7daa1ad
whocares <flevn@protonmail.com>
2025-02-03 16:52:49 +0100

    Forgot to upload these!

---
+++ code/xcaliber_geometry.h
@@ -0,0 +1,37 @@
+#ifndef XCALIBER_GEOMETRY_H
+#define XCALIBER_GEOMETRY_H
+
+#include "xcaliber_math.h"
+
+typedef struct xc_triangle
+{
+  xc_vec2i vertices[3];
+} xc_triangle;
+
+typedef struct xc_shaded_triangle
+{
+  xc_vec2i vertices[3];
+  xc_colour colours[3];
+} xc_shaded_triangle;
+
+typedef struct xc_line
+{
+  xc_vec2i start;
+  xc_vec2i end;
+} xc_line;
+
+typedef struct xc_quad
+{
+  /* bottom left */
+  xc_vec2i position;
+  int32_t width;
+  int32_t height;
+} xc_quad;
+
+typedef struct xc_circle
+{
+  xc_vec2i center;
+  int32_t radius;
+} xc_circle;
+
+#endif
---
+++ code/xcaliber_transform.h
@@ -0,0 +1,8 @@
+#ifndef XCALIBER_TRANSFORM_H
+#define XCALIBER_TRANSFORM_H
+
+#include "xcaliber_common.h"
+
+void xct_rotate (xcr_triangle, f32_t radians);
+
+#endif
